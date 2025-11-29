88ac8e8eafc2fd710e46333ff43674eceac5ebb6
Guang-Hai Liu <liuguanghai009@163.com>
2025-11-29 15:42:54 +0800

    Add files via upload

---
+++ package/Download_instructions.m
@@ -0,0 +1,11 @@
+%% Download instructions for required package files
+
+% Package files needed:
+% 1. imagenet_repyggplus_L2pse_deploy.mat
+% 2. RepVGG-epoch50-size448-dim640.mat  
+% 3. ResNet101-epoch50-size448-dim2048.mat
+% 4. VGG16-epoch50-size448-dim512.mat
+% 5. VIT_Large_dim1024.mat
+%
+% Download from: https://pan.quark.cn/s/0f4cfc6065d6
+% Place all files into the 'package' folder to run the program.
