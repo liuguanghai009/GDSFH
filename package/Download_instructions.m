31cda10ae7cae08b1e6bc1e2bc15da5d4fc46152
Guang-Hai Liu <liuguanghai009@163.com>
2025-11-30 11:15:23 +0800

    Update download instructions for package files

--- package/Download_instructions.m
+++ package/Download_instructions.m
@@ -1,11 +1,12 @@
-%% Download instructions for required package files
+% Download instructions for required package files
 
 % Package files needed:
-% 1. imagenet_repyggplus_L2pse_deploy.mat
-% 2. RepVGG-epoch50-size448-dim640.mat  
-% 3. ResNet101-epoch50-size448-dim2048.mat
-% 4. VGG16-epoch50-size448-dim512.mat
-% 5. VIT_Large_dim1024.mat
-%
-% Download from: https://pan.quark.cn/s/0f4cfc6065d6
+% 1. repvgg_package - RepVGG model retrained for 50-300 epochs
+% 2. ResNet101_package - ResNet101 model retrained for 50-300 epochs  
+% 3. vgg16_package - VGG16 model retrained for 50-300 epochs
+
+
+% Download from: https://pan.quark.cn/s/c019a292cd69
+
+% Instructions:
 % Place all files into the 'package' folder to run the program.
